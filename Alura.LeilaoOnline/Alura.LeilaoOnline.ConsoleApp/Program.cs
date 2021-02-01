6be3b4a7041fd7a17326f12b05b1e829ed964482
matheus.conde <rylander.tech@bume.io>
2021-02-01 18:56:35 -0300

    first commit bloco 2 curso

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        private static void Verifica(double esperado, double obtido)
+        {
+            var cor = Console.ForegroundColor;
+
+            if (esperado == obtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK " + obtido);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"TESTE FALHO! Esperado: {esperado}, obtido: {obtido}");
+            }
+            Console.ForegroundColor = cor;
+        }
+
+        static void Main()
+        {
+
+            //LeilaoComVariosLances();
+            //LeilaoComApenasUmLance();
+
+        }
+
+    }
+}
